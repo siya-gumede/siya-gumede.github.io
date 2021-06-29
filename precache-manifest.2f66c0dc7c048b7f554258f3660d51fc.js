d543416bd921553992c9479a256e3b51a8dfd841
siya-gumede <gumede.siyabonga4@gmail.com>
2021-06-29 13:35:40 +0200

    Updates

---
+++ precache-manifest.2f66c0dc7c048b7f554258f3660d51fc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d53478a91f02d1c44ce1969d4b043a45",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b059513cf2746c1aa9ec",
+    "url": "/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "94833f0d9aaa3be6210f",
+    "url": "/static/js/2.19e99e5d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.19e99e5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b059513cf2746c1aa9ec",
+    "url": "/static/js/main.9a458792.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "a9f4195e070ea88feecfdbd90af6a47c",
+    "url": "/static/media/resume.a9f4195e.pdf"
+  },
+  {
+    "revision": "a2dfbf49d776a708f18168c9de277ee4",
+    "url": "/static/media/siya.a2dfbf49.jpg"
+  }
+]);
